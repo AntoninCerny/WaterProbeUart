3a1e5eb082e782124fd36e69e718dd03cdbd2561
AntoninCerny <229309@vutbr.cz>
2024-07-01 19:39:34 +0200

    added helloworld

---
+++ hello_world.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h> // for sleep function
+
+int main() {
+    while (1) {
+        printf("hello%%world\n");
+        fflush(stdout); // Ensure the output is printed immediately
+        sleep(1); // Wait for 1 second
+    }
+    return 0; // This line will never be reached
+}
