7587693494dad105c51c4d2418b23cc5912922b6
AntoninCerny <229309@vutbr.cz>
2024-07-02 19:00:18 +0200

    chaged uart_main to wiringSerial library

--- uart_main.c
+++ uart_main.c
@@ -3,9 +3,10 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <errno.h>
-#include <termios.h>
+//#include <termios.h>
 //GPIO library
 #include <wiringPi.h>
+#include <wiringSerial.h>
 
 void setGpioPinHigh(int pin);
 
@@ -13,15 +14,16 @@ void setGpioPinHigh(int pin);
 int main() {
     setGpioPinHigh(5);
     int fd;
-    FILE *file;
+    
 
     // Open the Port. We want read/write, no "controlling tty" status, and open it no matter what state DCD is in
-    fd = open("/dev/ttyAMA0", O_RDWR | O_NOCTTY | O_NDELAY);
+    //fd = open("/dev/serial0", O_RDWR | O_NOCTTY | O_NDELAY);
+    fd = serialOpen("/dev/ttyAMA0", 9600)
     if (fd == -1) {
         perror("open_port: Unable to open /dev/ttyAMA0 - ");
         return -1;
     }
-
+    /*
     // Configure the port
     struct termios options;
     tcgetattr(fd, &options);
@@ -33,50 +35,31 @@ int main() {
     options.c_cflag &= ~CSIZE;           // Mask the character size bits
     options.c_cflag |= CS8;              // 8 data bits
     tcsetattr(fd, TCSANOW, &options);
+    */
 
-    // Open the file for writing
-    file = fopen("received_data.txt", "w");
-    if (file == NULL) {
-        perror("Unable to open file - ");
-        close(fd);
-        return -1;
-    }
-
-    // Read and write data
-    char buf[256];
+    //read data in endless loop
+    int bits;
     int n;
     while (1) {
-        n = read(fd, buf, sizeof(buf) - 1);
-        if (n < 0) {
-            if (errno == EAGAIN) {
-                // No data available right now, continue
-                usleep(100000); // Sleep for 100ms
-                continue;
-            } else {
-                perror("Read failed - ");
-                break;
+        bits = serialDataAvail(fd);
+        if(bits == 0){
+
+        }
+        else if(bits == -1){
+            printf("error reading serial")
+            serialFlush(fd);
+        }
+        else{
+            for(size_t i=0;i<bits;i++){
+                n = serialGetchar(fd);
+                printf("read: %d\n", n);
+                fflush(stdout);
             }
-        } else if (n == 0) {
-            // No data, continue
-            usleep(100000); // Sleep for 100ms
-            continue;
-        } else {
-            // Data read
-            buf[n] = '\0'; // Null-terminate the string
-            fprintf(file, "%s", buf);
-            fflush(file);
-            printf("%i bytes read: %s\n", n, buf);
         }
     }
-
-    // Don't forget to clean up
-    fclose(file);
     close(fd);
     return 0;
 }
-
-
-
 
 void setGpioPinHigh(int pin) {
     // Set up the wiringPi library
